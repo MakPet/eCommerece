9286d150f7315d1d2d5d4a3f0465bd8b6e276ed8
MakPet <valadok123@gmail.com>
2019-12-23 21:40:04 +0200

    Кнопка Up

--- js/script.js
+++ js/script.js
@@ -1,12 +1,39 @@
-$('.slider').slick({
-	prevArrow:"<div class='arrow arrow-prev'></div>",
-    nextArrow:"<div class='arrow arrow-next'></div>"
-});
+$(function() {
+	$('.slider').slick({
+		prevArrow:"<div class='arrow arrow-prev'></div>",
+	    nextArrow:"<div class='arrow arrow-next'></div>"
+	});
 
-document.body.addEventListener('click', function(e) {
-  if (e.target.matches('button')) {
-    console.log('btn!')
-    const icon = e.target.closest('div.add-to-inner').querySelector('.add-to .icon')
-    icon.classList.add('visible')
-  }
-})
+	document.body.addEventListener('click', function(e) {
+	  if (e.target.matches('button')) {
+	    console.log('btn!')
+	    const icon = e.target.closest('div.add-to-inner').querySelector('.add-to .icon')
+	    icon.classList.add('visible')
+	  }
+	})
+
+	function backToTop() {
+
+		let button = $('.up');
+
+		$(window).on('scroll', () => {
+			if ($(this).scrollTop() >= 668) {
+		  		button.fadeIn();
+			} else {
+			  button.fadeOut();
+			}
+		});
+
+		button.on('click', (e) => {
+			e.preventDefault();
+			$('html').animate({scrollTop: 0}, 1000);
+		})
+		}
+
+	backToTop();
+
+	$('.btn-menu').on('click', function(){
+		$('.main-menu').slideToggle();
+	});
+
+});
